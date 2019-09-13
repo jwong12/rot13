affbc74fccca6e9a27e8350526e3da068fc4fb93
James Wong <jwong_li@hotmail.com>
2019-09-13 01:18:33 -0700

    implement rot1 to rot26 mod26 using dom/javascript

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 window.onload = function() {
-	let result;
-	let alphabets = "abcdefghijklmnopqrstuvwxyz";
-	let button = document.getElementById('mybutton');
-	let select = document.querySelector('select');
-	let rot;
-	
+	const MOD_26 = 26;
+	const alphabets = "abcdefghijklmnopqrstuvwxyz";
+	const button = document.getElementById('convertButton');
+	const select = document.querySelector('select');
+
+	let createOptions = () => {
+		for(let i = 1; i <= MOD_26; i++) {
+			const option = document.createElement('option');
+			option.value = "rot" + i;
+			option.textContent = "Rot" + i;
+			select.appendChild(option);
+		}
+	}
+
+	createOptions();
+	select.selectedIndex = 12;
+	let rot = parseInt(select.value.slice(3));
+	const title = document.querySelector('h1')
+	title.textContent = "Rot" + rot + "  Mod26";
+
 	select.onchange = function() {
 		rot = parseInt(select.value.slice(3));
+		title.textContent = "Rot" + rot + "  Mod26";
 	}
 
 	button.onclick = function() {
-		result = "";
-		rotate();
+		let result = "";
+		rotate(result);
 	}
 	
-	function rotate() {
+	function rotate(result) {
 		let inputText = document.getElementById('inputarea').value;
 
 		for (let i = 0; i < inputText.length; i++) {
 			if(alphabets.includes(inputText[i]) === true) {
 				let index = alphabets.indexOf(inputText[i]);
-				let letter = alphabets[(index + rot) % 26];
+				let letter = alphabets[(index + rot) % MOD_26];
 				result += letter;
 
 			} else {
 				result += inputText[i];
 			}
 		}
+
 		let outputText = document.getElementById('outputarea');
 		outputText.value = result;
  	}
