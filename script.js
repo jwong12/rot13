2c6ddc506a4d488fd4cf519c5492c70efba2ab1a
James Wong <jwong_li@hotmail.com>
2019-09-13 00:31:07 -0700

    implement rot11, 13, 15

--- script.js
+++ script.js
@@ -2,19 +2,25 @@ window.onload = function() {
 	let result;
 	let alphabets = "abcdefghijklmnopqrstuvwxyz";
 	let button = document.getElementById('mybutton');
+	let select = document.querySelector('select');
+	let rot;
 	
+	select.onchange = function() {
+		rot = parseInt(select.value.slice(3));
+	}
+
 	button.onclick = function() {
 		result = "";
-		rot13();
+		rotate();
 	}
 	
-	function rot13() {
+	function rotate() {
 		let inputText = document.getElementById('inputarea').value;
 
 		for (let i = 0; i < inputText.length; i++) {
 			if(alphabets.includes(inputText[i]) === true) {
 				let index = alphabets.indexOf(inputText[i]);
-				let letter = alphabets[(index + 13) % 26];
+				let letter = alphabets[(index + rot) % 26];
 				result += letter;
 
 			} else {
